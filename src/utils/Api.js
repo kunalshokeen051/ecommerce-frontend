86ab5c378a48608bf2e677cf631c03e9fc0ebe56
kunalshokeen051 <119399583+kunalshokeen051@users.noreply.github.com>
2023-06-03 18:14:06 +0530

    fiels updated

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 const params = {
     headers: {
-        Authorization: "bearer " + process.env.REACT_APP_STRIPE_APP_KEY,
+        Authorization: "bearer " + process.env.REACT_APP_STRIPEAPPKEY,
     },
 };
 
 export const fetchDataFromApi = async (url) => {
     try {
         const { data } = await axios.get(
-            process.env.REACT_APP_DEV_URL + url,
+            process.env.REACT_APP_DEVURL + url,
             params
         );
         return data;
